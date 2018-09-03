d4120c5c93cc709783a347ea6352d4b803629766
zhangnanping <417614352@qq.com>
2018-09-03 18:35:28 +0800

    修改了一些出错的配置文件

---
+++ App/src/controller/UserController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class UserController {
+	//主页路径
+	@RequestMapping("/manager/login")
+	public String login(){
+		System.out.println(2);
+		return "backendlogin";
+	}
+	
+	
+}
